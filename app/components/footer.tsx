53733b771938d7c1c1ae0bbf0cbe551b8bc33d89
BrittGall <crlnfllr@gmail.com>
2023-07-12 14:18:24 -0500

    added responsiveness

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,10 +8,10 @@ export const Footer = () => {
                 <span className='text-yellow-600 font-black'>Fetch Doggy</span>
             </Link>
             <Link href={'https://github.com/brittgalloway'}>
-                <span className='font-black text-stone-700'>Brittney Galloway</span>
+                <span className='font-black text-stone-700 text-center'>Brittney Galloway</span>
             </Link>
-            <span className='text-stone-700'>2023</span>
-            <span className='text-stone-700'>Made with Next.js</span>
+            <span className='text-stone-700 text-center'>2023</span>
+            <span className='text-stone-700 text-right'>Made with Next.js</span>
         </footer>
     )
   }
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function HomePage() {
   return (
     <>
       <Header/>
-      <main className='flex flex-col items-center justify-center p-24'>
-        <h2 className='font-medium text-stone-700'>Find your best friend</h2>
+      <main className='flex flex-col'>
+        <h2 className='font-medium text-stone-700 mb-[20px]'>Find your best friend</h2>
         <div className='circular header shadow-md'>
           <h1 className='title text-yellow-600 font-black'>Fetch Doggy</h1>
         </div>
